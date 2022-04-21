9aa8333389ada79a8630472ae2b787c3bd5f8a07
OlegSokolkin <Oleg.Sokolkin@gmail.com>
2022-04-22 02:27:42 +0300

    1

--- test_product_page.py
+++ test_product_page.py
@@ -3,7 +3,8 @@ from selenium import webdriver
 #from .pages.main_page import MainPage
 from .pages.product_page import ProductPage
 
-link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+#link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 @pytest.fixture
 def browser():
@@ -13,9 +14,21 @@ def browser():
     print("\nquit browser..")
     browser.quit()
 
-def test_guest_should_see_login_link(browser): #заходим на страницу, тыкаем в кнопку "добавить в корзину"
+#Добавим немного параметризации нашему тесту. Задача - передать список ссылок, которые надо проверить на падения
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                                  pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6", marks=pytest.mark.xfail),
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+
+def test_guest_should_see_login_link(browser, link): #заходим на страницу, тыкаем в кнопку "добавить в корзину"
     #link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+    #link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     page = ProductPage(browser, link)
     page.open()
     page.should_be_add_to_cart() #только проверяем сам факт существования ссылки для добавления в корзину
